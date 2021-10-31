ab6458148868f51349becb84f8a39586e24de718
Des Barrett <des.barrett@gmail.com>
2021-10-31 13:16:05 +0000

    added XHR to get  bitcoin price

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+// Get dog image
+
 let dogPic = document.getElementById("photo");
 let getRndDogBtn = document.getElementById("dogBtn");
 
@@ -12,5 +14,27 @@ getRndDogBtn.addEventListener("click", function () {
   };
 
   XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+  XHR.send();
+});
+
+// Get Bitcoin Price using XHR
+let bitcoinApiUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
+let bitcoinBtnXhr = document.querySelector("#btn-bitcoin");
+let usdXhr = document.querySelector("#usd");
+let euroXhr = document.querySelector("#euro");
+
+bitcoinBtnXhr.addEventListener("click", function () {
+  let XHR = new XMLHttpRequest();
+
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState === 4 && XHR.status === 200) {
+      let bitcoinXhrPrice = JSON.parse(XHR.responseText);
+      console.log(bitcoinXhrPrice);
+      usdXhr.innerHTML = `USD: ${bitcoinXhrPrice.bpi.USD.rate}`;
+      euroXhr.innerHTML = `EURO: ${bitcoinXhrPrice.bpi.EUR.rate}`;
+    }
+  };
+
+  XHR.open("GET", bitcoinApiUrl);
   XHR.send();
 });
